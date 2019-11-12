14bf24cf6dc81d47be79bb0124912f969727a37f
moosedookie <moosedookie@moosedookie.me>
2019-11-12 06:04:11 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# docker-lftp-mirror
+- Description:
+  - Quick &amp; dirty SFTP mirror via LFTP
+
+- Instructions:
+  - Set required environmental variables
+
+- Variables:
+  - Required
+    - PUID: UserId used to run process / file ownership
+    - PGID: GroupId used to run process / file ownership
+    - HOST: IP/host/url of host
+    - PORT: Port of host
+    - USERNAME: SSH user on host
+    - PASSWORD: Pass for user on host
+    - REMOTE_DIR: Directory on host to mirror locally
+    - LFTP_PARTS: Number of parts in which to split files (-use-pget[-n=N])
+    - LFTP_FILES: Number of files to download in parallel (--parallel[=N])
+  - Optional
+    - FINISHED_DIR: Optional finished directory to place finished transfer (by default, downloads end up in /config/download)
---
+++ lftp-mirror.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Display variables for troubleshooting
+echo -e "Variables set:\\n\
+PUID=${PUID}\\n\
+PGID=${PGID}\\n\
+HOST=${HOST}\\n\
+PORT=${PORT}\\n\
+USERNAME=${USERNAME}\\n\
+PASSWORD=${PASSWORD}\\n\
+REMOTE_DIR=${REMOTE_DIR}\\n\
+FINISHED_DIR=${FINISHED_DIR}\\n\
+LFTP_PARTS=${LFTP_PARTS}\\n\
+LFTP_FILES=${LFTP_FILES}\\n"
+
+# if no finished files directory specified, default to /config/download
+[ -z "$FINISHED_DIR" ] && FINISHED_DIR="/config/download"
+
+# create a directory for active downloads
+mkdir -p /config/.download
+
+# create finished downloads directory
+mkdir -p /config/download
+
+while true
+do
+	# LFTP with specified segment & parallel
+	echo "[$(date '+%H:%M:%S')] Checking ${REMOTE_DIR} for files....."
+
+	lftp -u $USERNAME,$PASSWORD sftp://$HOST -p $PORT <<-EOF
+        set ssl:verify-certificate no
+        set sftp:auto-confirm yes
+	    mirror -c --no-empty-dirs --Remove-source-files --Remove-source-dirs --use-pget-n=$LFTP_PARTS -P$LFTP_FILES $REMOTE_DIR /config/.download
+	quit
+	EOF
+
+    if [ "$(ls -A /config/.download)" ]
+    then
+    	# Move finished downloads to destination directory
+    	echo "[$(date '+%H:%M:%S')] Moving files....."
+
+	chmod -R 777 /config/.download/*
+        mv -fv /config/.download/* $FINISHED_DIR
+    else
+        echo "[$(date '+%H:%M:%S')] Nothing to download"
+    fi
+
+    # Repeat process after one minute
+    echo "[$(date '+%H:%M:%S')] Sleeping for 1 minute"
+    sleep 1m
+done
